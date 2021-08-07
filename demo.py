b781c59575482034a08cce3dd3eed3356cfeeaf3
lysenkorfe <88564971+lysenkorfe@users.noreply.github.com>
2021-08-07 03:51:13 +0300

    Update demo.py

--- demo.py
+++ demo.py
@@ -13,5 +13,5 @@ def remove_punctuation(str):
 if __name__=='__main__':
     # To take input from the user
     # my_str = input("Enter a string: ")
-    my_str = "Hello!!!, he said ---and went."
+    my_str = "Hello!, he said ---and went."
     remove_punctuation(my_str)
